4b9ea524515455f5fd38ced8744455083314174b
poula9744 <poula9744@naver.com>
2023-12-26 13:09:20 +0900

    문제 38번 수정

--- src/com/javaex/practice/Ex38.java
+++ src/com/javaex/practice/Ex38.java
@@ -1,9 +1,21 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex38 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("전체금액을 입력해주세요: ");
+		int money = sc.nextInt();
+		
+		int realmoney = money;
+		
+		System.out.println("실제지불금액은 " + money + " 입니다.");
+		
+		sc.close();
 
 	}
 
