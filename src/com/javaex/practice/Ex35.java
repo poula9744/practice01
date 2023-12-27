76e0eba631f201e340406050f92efa33adb2f59e
poula9744 <poula9744@naver.com>
2023-12-27 10:17:01 +0900

    Merge branch 'master' of https://github.com/poula9744/practice01.git

--- src/com/javaex/practice/Ex35.java
+++ src/com/javaex/practice/Ex35.java
@@ -8,7 +8,7 @@ public class Ex35 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		long light = 300000L*60*60*24*365;
+		long light = 300000L*60*60*24*365; //곱하다 보면 int를 넘어감. 처음부터 long형으로 크게 만들면 정상값
 		
 		System.out.println("빛이 1년 동안 가는 거리는 " + light + " km 입니다.");
 		
